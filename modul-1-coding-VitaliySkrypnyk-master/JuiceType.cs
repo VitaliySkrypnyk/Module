1d55204fb389b1440915ffe73fdcf1be0b1861cc
VitaliySkrypnyk <149414693+VitaliySkrypnyk@users.noreply.github.com>
2023-10-30 20:34:36 +0200

    Add files via upload

---
+++ modul-1-coding-VitaliySkrypnyk-master/JuiceType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul
+{
+    // TODO 1: Create the JuiceType enumeration.
+    public enum JuiceType
+    {
+        UNKNOWN,
+        APPLE,
+        ORANGE,
+        TOMATO
+    }
+
+   
+}
